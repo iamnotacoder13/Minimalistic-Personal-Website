688a093583a8b9888e48cb27afcc2970c4289780
iamnotacoder13 <masondferre@gmail.com>
2026-02-15 11:30:39 -0500

    Update all organization descriptions with new concise versions

--- src/components/Organizations.tsx
+++ src/components/Organizations.tsx
@@ -5,30 +5,30 @@ export const Organizations = () => {
   const organizations = [{
     name: 'Arch',
     period: '2025 - 2026',
-    description: 'Arch is a Series B fintech startup that is working to automate the post-investment process for alts investors. I sat on the operations team, onboarding clients and ensuring the business ticked. I left Arch early 2026 to pursue my own ventures.',
+    description: 'Worked on automating the post-investment process for alts investors. Had the itch to do my own thing. Left in 2026.',
     logo: 'https://raw.githubusercontent.com/iamnotacoder13/images/main/download.jpeg',
     website: 'https://arch.co/'
   },{
     name: 'Taob Holdings',
     period: '2022 - Present',
-    description: 'I am on the Board of Directors of Taob Holdings, LLC, a family-owned asset management company. My roles comprise identifying investment property leads, underwriting deals, and networking with other professionals. In our first year, I led a portfolio growth of 20%. We have investments across the PNW and Nashville, Tennessee. I look at real estate as a puzzle waiting to be pieced together, as each property of a portfolio needs to fit together to create a beautiful picture/successful portfolio.',
+    description: 'Growing a small real estate portfolio. Focused on multi-family and small scale commercial units across the PNW and Nashville area.',
     logo: 'https://raw.githubusercontent.com/iamnotacoder13/images/refs/heads/main/Screenshot%202023-11-05%20at%2012_15_38%20PM.avif'
   }, {
     name: 'Oregon Consulting Group',
     period: '2022 - 2025',
-    description: 'I worked with the Oregon Consulting Group (OCG) for two and a half years as a Consultant, Account Manager, Project Manager, Senior Manager, and President. The OCG is a student-run strategy consulting firm housed out of the Lundquist College of Business at the University of Oregon. I have been staffed on multiple projects with amazing clients, focusing on business strategy and expansion. My past clients include a healthcare per diem staffing startup, an electrochemistry lab startup, and a >$1B AUM regional bank. Under my leadership, our group landed a US-leading Kombucha manufacturer and a $3B+ valued lithium-ion battery material producer as clients.',
+    description: 'Some of the most fun I have had in my life. Worked across health tech, electrochemistry, and banking industries. Spent a lot of early mornings and late nights working on decks, chugging coffee, and laughing. OCG <3',
     logo: 'https://github.com/iamnotacoder13/images/blob/main/Screenshot_2025-10-17_at_4.17.44_PM-removebg-preview.png?raw=true',
     website: 'https://oregonconsultinggroup.com'
   }, {
     name: 'July AI',
     period: '2025 - 2025',
-    description: 'I led growth for July AI, a pre-seed startup that was founded to ensure that everyone can adapt to these changes successfully. During my tenure, we launched our alpha product on the University of Oregon campus, where I spearheaded all user acquisiton.',
+    description: 'Led growth for July AI to help students and knowlege workers prepare for the AI transition in the workforce. Launched alpha product during my tenure.',
     logo: 'https://raw.githubusercontent.com/iamnotacoder13/images/refs/heads/main/july_JPG.avif',
     website: 'https://gojuly.ai'
   }, {
     name: 'Beer Me.',
     period: '2024-2025',
-    description: 'My roommate and I co-founded Beer Me., a hoodie that holds your beer. We received $5K in funding from the Oregon Innovation Challenge to launch our first collection of hoodies. I helped lead operations, sales, and marketing, leading to 200+ sold hoodies. In May 2025 we sold the company.',
+    description: 'Co-founded Beer Me. with my roommate and best friend. Sold hundreds of hoodies through in-person tactics. Found it is easier to sell after shotgunning one brew.',
     logo: 'https://raw.githubusercontent.com/iamnotacoder13/images/refs/heads/main/Black%20Logo.avif',
     website: 'https://shopbeerme.com/'
   }];
